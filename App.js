da2080eb0dd1476e5717e3c100ac002f90067b62
Farhan Akbar <farhanaan2112@gmail.com>
2024-11-25 18:52:14 +0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const CenteredTextWithBoxes = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Aku Sigma Kamu Ligma!!</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D3D3D3', 
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 50, 
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%', 
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'white', 
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'cyan', 
   },
 });
+
+export default CenteredTextWithBoxes;
